d537415614e1cc231d71651da80b6e7afce40267
Timothy Barasa <timbaros87@gmail.com>
2024-04-20 03:36:23 +0300

    Create Ramens Rater

--- src/index.js
+++ src/index.js
@@ -1,29 +1,87 @@
 // index.js
 
-// Callbacks
-const handleClick = (ramen) => {
-  // Add code
+export const url = "http://localhost:3000/ramens";
+
+export const main = () => {
+  // Wait for the DOM to fully load before executing the JavaScript code
+  document.addEventListener("DOMContentLoaded", () => {
+    // Fetch the data from the server and render the ramens
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((ramens) => renderRamens(ramens));
+
+    // Select the new-ramen form
+    const newRamenForm = document.querySelector("#new-ramen");
+
+    // Attach the addSubmitListener function as the event listener for form submission
+    newRamenForm.addEventListener("submit", addSubmitListener);
+  });
 };
 
-const addSubmitListener = () => {
-  // Add code
-}
+export const renderRamens = (ramens) => {
+  const ramenMenu = document.querySelector("#ramen-menu");
 
-const displayRamens = () => {
-  // Add code
+  ramens.forEach((ramen) => {
+    const image = document.createElement("img");
+    image.src = ramen.image;
+    ramenMenu.appendChild(image);
+
+    image.addEventListener("click", () => renderDetails(ramen));
+  });
 };
 
-const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
-}
+export const renderDetails = (ramen) => {
+  const ramenImage = document.querySelector(".detail-image");
+  ramenImage.src = ramen.image;
 
-main()
+  const ramenName = document.querySelector(".name");
+  ramenName.innerText = "Name: " + ramen.name;
 
-// Export functions for testing
-export {
-  displayRamens,
-  addSubmitListener,
-  handleClick,
-  main,
+  const ramenRestaurant = document.querySelector(".restaurant");
+  ramenRestaurant.innerText = "Restaurant: " + ramen.restaurant;
+
+  const ramenRating = document.querySelector("#rating-display");
+  ramenRating.innerText = "Rating: " + ramen.rating;
+
+  const ramenComment = document.querySelector("#comment-display");
+  ramenComment.innerText = "Comment: " + ramen.comment;
 };
+
+export const addSubmitListener = (event) => {
+  event.preventDefault();
+
+  // Get form input values
+  const newName = document.getElementById("new-name").value;
+  const newRestaurant = document.getElementById("new-restaurant").value;
+  const newImage = document.getElementById("new-image").value;
+  const newRating = document.getElementById("new-rating").value;
+  const newComment = document.getElementById("new-comment").value;
+
+  // Create a new ramen object
+  const newRamen = {
+    name: newName,
+    restaurant: newRestaurant,
+    image: newImage,
+    rating: newRating,
+    comment: newComment,
+  };
+
+  // Render the new ramen
+  renderRamen(newRamen);
+
+  // Reset the form
+  event.target.reset();
+};
+
+export const renderRamen = (ramen) => {
+  const image = document.createElement("img");
+  image.src = ramen.image;
+
+  const ramenMenu = document.querySelector("#ramen-menu");
+  ramenMenu.appendChild(image);
+
+  image.addEventListener("click", () => renderDetails(ramen));
+};
+
+// Invoke the main function
+main();
